20f8cff96aae39fd3b8ca36cd090fb91ef97d24e
maccza <mc180996@wp.pl>
2019-10-15 11:44:07 +0200

    xml parse

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,22 +1,31 @@
 import csv
 import time
-#from xml.dom import minidom
-#settings = minidom.parse('lab1.xml')
-#itemlist = settings.getElementsByTagName('item')
-#a = settings.childNodes[0]
-#print(itemlist)
-#print (a.firstChild.nodeValue)
+from xml.dom import minidom
+xml_ = minidom.parse('lab1.xml')
+settings_tree = xml_.documentElement
+settings = settings_tree.getElementsByTagName('items')
+time_ = 0
+file_name = '' 
+for setting in settings:
+	par_time = setting.getElementsByTagName('time')[0]
+	time_= int(par_time.childNodes[0].data)
+	par_file = setting.getElementsByTagName('file_name')[0]
+	file_name = par_file.childNodes[0].data
+    
+print(time_)
+print(file_name)
 
-entropy  = open("/home/students/mgr2018/maccza/Desktop/KiBdF/entropy.txt", "r")
-value = []
-i = 0
+entropy = []
+
 while (True):
 	entropy_avile  = open("/proc/sys/kernel/random/entropy_avail", "r")
 	for x in entropy_avile:
-			value.append(int(x))
+			entropy.append(int(x))
 	entropy_avile.close()
-	with ("
+	#with ("
 	time.sleep(4)
-print(value)	
+	print(x)
+	entropy.pop()
+	
 			
 
