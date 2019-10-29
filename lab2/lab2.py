c1f245d737bcf1b0443a80ec8183c4a3c45e6d2b
maccza <mc180996@wp.pl>
2019-10-29 13:19:07 +0100

    task2_

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -5,11 +5,11 @@ class LCG:
     def change_seed(self,new_seed):
         self.seed = new_seed
         self.last_results = self.seed
-    def lcg(self,par_a,par_c,par_m):
+    def lcg(self,par_a,par_m):
         pseudo_random_number = (par_a*self.last_results+1)%par_m
         self.last_results = pseudo_random_number
         return pseudo_random_number
 if __name__ == "__main__":
-    lcg = LCG(1000000)
+    lcg = LCG(321342345434213235325324125436754645645645645645654)
     for i in range(100):
-        print(lcg.lcg(345,125,677))
+        print(lcg.lcg(345,67745655464553523543465375465464))
