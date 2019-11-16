8d51801af8c1544b63d649de2b6f2e7bfcfda565
Denys Morokov <morokov.denis@gmail.com>
2019-11-16 12:20:31 +0100

    lab3: dokonczony skrypt, lista kluczy

--- lab3/zadanie3_czarnecki_morokov.py
+++ lab3/zadanie3_czarnecki_morokov.py
@@ -1,23 +1,39 @@
-import socket, ssl
-import OpenSSL
+import ssl
 from OpenSSL import crypto
-import re
+from xml.dom import minidom
+
+def upload_public_key():
+    xmldoc = minidom.parse('zadanie3_domens.xml')
+    itemlist = xmldoc.getElementsByTagName('domena')
+    text_file = open("klucze_publiczne.txt", "w")
+    keys = []
+    for domena in itemlist:
+        hostname = str(domena.attributes['id'].value)
+        print(hostname)
+        port = 443
+        cert = ssl.get_server_certificate((hostname, port))
+        crtObj = crypto.load_certificate(crypto.FILETYPE_PEM, cert)
+        pubKeyObject = crtObj.get_pubkey()
+        pubKeyString = crypto.dump_publickey(crypto.FILETYPE_PEM, pubKeyObject)
+        pubkey = pubKeyString.decode("utf-8")
+        text_file.write(hostname)
+        text_file.write("\n")
+        text_file.write(pubkey)
+        text_file.write("\n")
+        keys.append(pubkey)
+    text_file.close()
+    return keys
 
 
-hostname = 'www.pw.edu.pl'
-port = 443
-cert = ssl.get_server_certificate((hostname, port))
+def moooeeeep(seq):
+    seen = set()
+    seen_add = seen.add
+    # adds all elements it doesn't know yet to seen and all other to seen_twice
+    seen_twice = set(x for x in seq if x in seen or seen_add(x))
+    # turn the set into a list (as requested)
+    return list(seen_twice)
 
 
-
-#cert is the encrypted certificate int this format -----BEGIN -----END
-crtObj = crypto.load_certificate(crypto.FILETYPE_PEM, cert)
-pubKeyObject = crtObj.get_pubkey()
-pubKeyString = crypto.dump_publickey(crypto.FILETYPE_PEM,pubKeyObject)
-pubKeyString = str(pubKeyString)
-# wynik = re.match(r'-----BEGIN PUBLIC KEY-----\n(.*)==\n-----END PUBLIC KEY-----\n', pubKeyString)
-# wynik = re.match(r''(.*)'', pubKeyString)
-# print(wynik)
-wynik = pubKeyString[0]
-print(cert)
-print(pubKeyString)
+if __name__ == '__main__':
+    out = upload_public_key()
+    print(moooeeeep(out))
